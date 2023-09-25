7f8750d7d224435a8295394e54b4ef80ec807f22
annisyaadell <annisyaadellyta10@gmail.com>
2023-09-25 16:20:36 +0700

    Menambahkan Folder pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Annisya Adellyta <br>
+            Nim saya: 19220896 <br>
+            kelas saya:19.3A.26 <br>
+            hobi saya:berenang";
+    }
+}
